f991f9ccdb257db97f821b04993865b981afbc1e
Javier Aguilar <javieragustinaguilar@gmail.com>
2024-04-13 18:25:53 -0300

    Se creo el server en el puerto 7000

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+//settings
+require('dotenv').config();
+app.set('port', process.env.PORT || 3000);
+
+
+//midlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+//routes
+
+
+
+//static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+//starting the server
+app.listen(app.get('port'), ()=>{
+    console.log(`server on port ${app.get('port')}`)
+});
 
 
