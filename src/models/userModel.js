d475f7b2a1de45071d0a2b50791a3ef18d358e64
Javier Aguilar <javieragustinaguilar@gmail.com>
2024-04-16 19:31:06 -0300

    Se modifico userModel y se agregaron los campos de name y username

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,21 +2,35 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    username:{
+        type: String,
+        required: [true, 'Por favor ingrese un nombre de usuario'],
+        unique: true,
+        trim: true
+    },
+
     email:{
         type: String,
         required: true, 
         unique: true,
-        match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        trim: true,
+        match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, 'Por favor ingrese un correo valido']
     },
 
     password:{
         type: String,
-        required: [true, 'La contraseña es requerida.'];
+        required: [true, 'La contraseña es requerida.']
     },
 
     role:{
         type: String,
-        required: [true, 'El rol es requerido.'];
+        required: [true, 'El rol es requerido.']
     }
 });
 
