9dea208f32537c1b5afdd47a7926ec422ebe71a0
Javier Aguilar <javieragustinaguilar@gmail.com>
2024-04-16 20:30:53 -0300

    Se creo el modelo de datos para los productos

---
+++ src/models/productModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const productSchema = new Schema({
+    title:{
+        type: String,
+        required: true, 
+        unique: true, 
+        trim: true
+    },
+
+    description:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    category:{
+        type: String,
+        required: true, 
+        enum: ['Carne', 'Pollo', 'Vegetarianas']
+    },
+
+    stock:{
+        type: Number,
+        required: true,
+        min: 0
+    },
+
+    controlStock:{
+        timestamp:{
+            type: Date,
+            default: Date.now
+        }
+    }
+});
+
+const productModel = mongoose.model('product', productSchema);
+
+module.exports = productModel;
